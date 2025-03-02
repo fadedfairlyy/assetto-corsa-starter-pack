ead11c5d690d61a004bbd20c318c5d037875736c
fadedfairlyy <fadedfairlyreal@gmail.com>
2025-03-02 13:59:27 -0600

    Update RunAssettoStarterPack.bat

--- RunAssettoStarterPack.bat
+++ RunAssettoStarterPack.bat
@@ -2,7 +2,7 @@
 echo Downloading and running Assetto Corsa starter pack script...
 
 REM Download the PowerShell script to a temporary location
-powershell -Command Invoke-WebRequest -Uri 'httpsraw.githubusercontent.comyourusernameyourrepomainassetto_corsa_starter_pack.ps1' -OutFile '$envTEMPassetto_corsa_starter_pack.ps1'
+powershell -Command Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/fadedfairlyy/assetto-corsa-starter-pack/refs/heads/main/assetto_corsa_starter_pack.ps1' -OutFile '$envTEMPassetto_corsa_starter_pack.ps1'
 
 REM Run the script with PowerShell
 powershell -NoExit -Command Start-Process powershell -ArgumentList '-NoExit', '-File', '$envTEMPassetto_corsa_starter_pack.ps1' -Verb RunAs
